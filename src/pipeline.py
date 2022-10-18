5c9a66222b367ac78d2e276fd2e47da7fde024ac
Dhruv Awasthi <dhruvawasthicc@gmail.com>
2022-10-18 22:06:14 +0530

    Update pipeline workflow

--- src/pipeline.py
+++ src/pipeline.py
@@ -2,6 +2,8 @@ import logging
 from datetime import datetime
 from typing import NoReturn
 
+from pandas.core.frame import DataFrame
+
 from src.helpers import load_dataset
 
 logger = logging.getLogger(__name__)
@@ -11,26 +13,39 @@ class Pipeline:
     def __init__(self, config_info):
         self.config_info = config_info
 
-    def preprocess_data(self):
+    def preprocess_data(
+        self,
+        df: DataFrame,
+    ):
         return
 
     def build_model(self):
         return
 
-    def train_model(self):
+    def train_model(
+        self,
+        train_df: DataFrame,
+    ):
         return
 
-    def test_model(self):
+    def test_model(
+        self,
+        test_df: DataFrame,
+    ):
         return
 
-    def train_and_test_model(self):
+    def train_and_test_model(
+        self,
+        train_df: DataFrame,
+        test_df: DataFrame,
+    ):
         return
 
     def run(
         self,
-        train=False,
-        test=False,
-        train_and_test=False,
+        train: bool = False,
+        test: bool = False,
+        train_and_test: bool = False,
     ) -> NoReturn:
 
         if train:
@@ -41,6 +56,9 @@ class Pipeline:
             except Exception as e:
                 logger.info(f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')} failed to load train dataset")
                 logger.info(f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')} error caused - {str(e)}")
+                return
+
+            self.train_model(train_df)
 
         elif test:
             try:
@@ -50,6 +68,9 @@ class Pipeline:
             except Exception as e:
                 logger.info(f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')} failed to load train dataset")
                 logger.info(f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')} error caused - {str(e)}")
+                return
+
+            self.test_model(test_df)
 
         elif train_and_test:
             try:
@@ -62,4 +83,7 @@ class Pipeline:
             except Exception as e:
                 logger.info(f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')} failed to load datasets")
                 logger.info(f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')} error caused - {str(e)}")
+                return
+
+            self.train_and_test_model(train_df, test_df)
         return

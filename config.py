98184e0c2b3ae2c33a69bd3be6255ecf15f2bbf5
Dhruv Awasthi <dhruvawasthicc@gmail.com>
2022-10-23 17:49:52 +0530

    Checkpoint 1 completed

--- config.py
+++ config.py
@@ -18,16 +18,16 @@ paths["test_data_path"] = os.path.join(DATASET_DIR, "test_data.csv")
 preprocess_configuration = dict()
 preprocess_configuration["drop_columns_below_std"] = 0.02
 preprocess_configuration["dimension_for_hashing"] = 16
-preprocess_configuration["train_hash_encoder"] = False
+preprocess_configuration["train_hash_encoder"] = True
 
 # set configuration for handling outliers
 preprocess_configuration["outliers"] = dict()
 preprocess_configuration["outliers"]["percentile_range"] = (10, 90)
-preprocess_configuration["outliers"]["scaled_factor"] = 1.7
+preprocess_configuration["outliers"]["scaled_factor"] = 1.5
 
 # set configuration for machine learning model
 model_configuration = dict()
 model_configuration["random_state"] = 42
 model_configuration["test_size"] = 0.2
 model_configuration["num_stratified_shuffle_splits"] = 5
-model_configuration["max_iter"] = 1000
+model_configuration["max_iter"] = 3000

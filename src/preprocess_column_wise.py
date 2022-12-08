d9d98745a7acf936250d6bf53826137aeacfc542
Dhruv Awasthi <dhruvawasthicc@gmail.com>
2022-12-09 04:47:47 +0530

    EDA done: REGION_POPULATION_RELATIVE

--- src/preprocess_column_wise.py
+++ src/preprocess_column_wise.py
@@ -6,62 +6,45 @@ from pandas.core.frame import DataFrame
 
 from src.helpers import col_info
 
-
 logger = logging.getLogger(__name__)
 
 
 def column_wise(
-    train_df: DataFrame,
-    test_df: DataFrame,
+        train_df: DataFrame,
+        test_df: DataFrame,
 ) -> tuple[DataFrame, DataFrame]:
+    # -------------------------------------------------------------------------
     # CNT_CHILDREN
     # train data
     # replace all values with cnt_children >=3 with 3
     column_name = "CNT_CHILDREN"
     logger.info(f"Preprocessing column: {column_name}")
     train_df[column_name].replace([3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 19], 3,
-                               inplace=True)
+                                  inplace=True)
 
     # test data
-    test_df[column_name].replace([3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19], 3,
-                                     inplace=True)
+    test_df[column_name].replace(
+        [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19], 3,
+        inplace=True)
+    # -------------------------------------------------------------------------
 
+    # -------------------------------------------------------------------------
     # AMT_INCOME_TOTAL
     # train data
     # there are just four samples with income >9000000; let's just drop them
     column_name = "AMT_INCOME_TOTAL"
     logger.info(f"Preprocessing column: {column_name}")
-    index_of_outliers, less_than_mean, more_than_mean, unique_values, iqr_range, lower_bound, upper_bound, column_mean_without_outliers = col_info(train_df, column_name)
+    index_of_outliers, less_than_mean, more_than_mean, unique_values, iqr_range, lower_bound, upper_bound, column_mean_without_outliers = col_info(
+        train_df, column_name)
     to_delete_indices = train_df[(train_df[column_name] >= 9000000)].index
     train_df.drop(index=to_delete_indices, inplace=True)
     index_of_outliers = index_of_outliers.drop(to_delete_indices)
     # trim outlier values with the extreme value that is not an outlier
     lowest_outlier_value = more_than_mean[0]
     index_lowest_outlier_value = \
-    np.where(unique_values == lowest_outlier_value)[0][0]
-    logger.info(f"Lowest outlier value in unique values: {lowest_outlier_value}")
-    logger.info(
-        f"Index of lowest outlier value in unique values: {index_lowest_outlier_value}")
-    logger.info(
-        f"Outliers will be trimmed to: {unique_values[index_lowest_outlier_value - 1]}")
-    train_df.loc[index_of_outliers, column_name] = unique_values[index_lowest_outlier_value - 1]
-
-    # test data
-    index_of_outliers = test_df[
-        (test_df[column_name] > upper_bound) | (
-                test_df[column_name] < lower_bound)].index
-    test_df.loc[index_of_outliers, column_name] = unique_values[index_lowest_outlier_value - 1]
-
-
-    # AMT_CREDIT
-    # train data
-    column_name = "AMT_CREDIT"
-    logger.info(f"Preprocessing column: {column_name}")
-    index_of_outliers, less_than_mean, more_than_mean, unique_values, iqr_range, lower_bound, upper_bound, column_mean_without_outliers = col_info(train_df, column_name)
-    lowest_outlier_value = more_than_mean[0]
-    index_lowest_outlier_value = \
         np.where(unique_values == lowest_outlier_value)[0][0]
-    logger.info(f"Lowest outlier value in unique values: {lowest_outlier_value}")
+    logger.info(
+        f"Lowest outlier value in unique values: {lowest_outlier_value}")
     logger.info(
         f"Index of lowest outlier value in unique values: {index_lowest_outlier_value}")
     logger.info(
@@ -75,13 +58,42 @@ def column_wise(
                 test_df[column_name] < lower_bound)].index
     test_df.loc[index_of_outliers, column_name] = unique_values[
         index_lowest_outlier_value - 1]
+    # -------------------------------------------------------------------------
 
+    # -------------------------------------------------------------------------
+    # AMT_CREDIT
+    # train data
+    column_name = "AMT_CREDIT"
+    logger.info(f"Preprocessing column: {column_name}")
+    index_of_outliers, less_than_mean, more_than_mean, unique_values, iqr_range, lower_bound, upper_bound, column_mean_without_outliers = col_info(
+        train_df, column_name)
+    lowest_outlier_value = more_than_mean[0]
+    index_lowest_outlier_value = \
+        np.where(unique_values == lowest_outlier_value)[0][0]
+    logger.info(
+        f"Lowest outlier value in unique values: {lowest_outlier_value}")
+    logger.info(
+        f"Index of lowest outlier value in unique values: {index_lowest_outlier_value}")
+    logger.info(
+        f"Outliers will be trimmed to: {unique_values[index_lowest_outlier_value - 1]}")
+    train_df.loc[index_of_outliers, column_name] = unique_values[
+        index_lowest_outlier_value - 1]
 
+    # test data
+    index_of_outliers = test_df[
+        (test_df[column_name] > upper_bound) | (
+                test_df[column_name] < lower_bound)].index
+    test_df.loc[index_of_outliers, column_name] = unique_values[
+        index_lowest_outlier_value - 1]
+    # -------------------------------------------------------------------------
+
+    # -------------------------------------------------------------------------
     # AMT_ANNUITY
     # train data
     column_name = "AMT_ANNUITY"
     logger.info(f"Preprocessing column: {column_name}")
-    index_of_outliers, less_than_mean, more_than_mean, unique_values, iqr_range, lower_bound, upper_bound,column_mean_without_outliers = col_info(train_df, column_name)
+    index_of_outliers, less_than_mean, more_than_mean, unique_values, iqr_range, lower_bound, upper_bound, column_mean_without_outliers = col_info(
+        train_df, column_name)
     # there is one sample with amt_annuity >= 258025.5; let's just drop it
     to_delete_indices = train_df[(train_df[column_name] >= 258025.5)].index
     train_df.drop(index=to_delete_indices, inplace=True)
@@ -108,8 +120,9 @@ def column_wise(
     test_df.loc[index_of_outliers, column_name] = unique_values[
         index_lowest_outlier_value - 1]
     test_df[column_name].fillna(column_mean_without_outliers, inplace=True)
+    # -------------------------------------------------------------------------
 
-
+    # -------------------------------------------------------------------------
     # AMT_GOODS_PRICE
     # train data
     column_name = "AMT_GOODS_PRICE"
@@ -142,10 +155,40 @@ def column_wise(
     test_df.loc[index_of_outliers, column_name] = unique_values[
         index_lowest_outlier_value - 1]
     test_df[column_name].fillna(column_mean_without_outliers, inplace=True)
+    # -------------------------------------------------------------------------
 
-
+    # -------------------------------------------------------------------------
     # REGION_POPULATION_RELATIVE
     # train data
     column_name = "REGION_POPULATION_RELATIVE"
+    logger.info(f"Preprocessing column: {column_name}")
+    index_of_outliers, less_than_mean, more_than_mean, unique_values, iqr_range, lower_bound, upper_bound, column_mean_without_outliers = col_info(
+        train_df, column_name)
+    # trim outlier values with the extreme value that is not an outlier
+    # fact - all the outliers here contain the same value
+    lowest_outlier_value = more_than_mean[0]
+    index_lowest_outlier_value = \
+        np.where(unique_values == lowest_outlier_value)[0][0]
+    logger.info(
+        f"Lowest outlier value in unique values: {lowest_outlier_value}")
+    logger.info(
+        f"Index of lowest outlier value in unique values: {index_lowest_outlier_value}")
+    logger.info(
+        f"Outliers will be trimmed to: {unique_values[index_lowest_outlier_value - 1]}")
+    train_df.loc[index_of_outliers, column_name] = unique_values[
+        index_lowest_outlier_value - 1]
+
+    # test data
+    index_of_outliers = test_df[
+        (test_df[column_name] > upper_bound) | (
+                test_df[column_name] < lower_bound)].index
+    test_df.loc[index_of_outliers, column_name] = unique_values[
+        index_lowest_outlier_value - 1]
+    # -------------------------------------------------------------------------
+
+    # -------------------------------------------------------------------------
+    # DAYS_BIRTH
+    column_name = "DAYS_BIRTH"
+    logger.info(f"Preprocessing column: {column_name}")
 
     return train_df, test_df
